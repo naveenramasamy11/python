4c846792da1a48b1e387fbe6b48b614f84a509cb
Naveen <naveenramasamy11@gmail.com>
2019-05-28 22:55:23 +0530

    python learning

--- list.py
+++ list.py
@@ -9,3 +9,18 @@ print("Change Item Value")
 a[0]=0
 print(a)
 
+print("file lenghth using len method")
+tmp=len(a)
+print(tmp)
+
+print("Append")
+a.append(5)
+print(a)
+
+print("Insert")
+a.insert(0,1)
+print(a)
+
+print("Remove")
+a.del(0)
+print(a)

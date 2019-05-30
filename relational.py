6f438d13939d7cc256cbf428eeb2503ad0d3793b
Naveen <naveenramasamy11@gmail.com>
2019-05-30 05:51:50 +0530

    python learning

---
+++ relational.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.7
+a = 20
+response = input()
+if int(response) < a:
+  print("It is less than a")
+elif int(response) > a:
+  print("It is greater than a")
+else:
+  print("It is equal")

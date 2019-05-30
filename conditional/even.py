f8ade2a8de6c8189a0e57e4096159dce0e6e26cf
Naveen <naveenramasamy11@gmail.com>
2019-05-30 08:29:52 +0530

    python learning

---
+++ conditional/even.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.7
+n=int(input("Enter the even numbers upto n values:\n"))
+
+print("The possible even numbers in the range:\n")
+for i in range(0,n,2):
+  print(i)
---
+++ conditional/pattern.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+n = 5
+for i in range(5):
+  for j in range(i):
+    print("* ", end="")
+  print("")
+
+for i in range(n,0, -1):
+  for j in range(i):
+    print("* ", end="")
+  print("")

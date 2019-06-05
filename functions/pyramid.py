4277d0c511acfbbd7ec6e23b5911c6e2c2399647
Naveen <naveenramasamy11@gmail.com>
2019-06-05 21:01:19 +0530

    learning functions

---
+++ functions/pyramid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.7
+def pyramid(x):
+    for i in range(x):
+        for k in range(x-i,-1,-1):
+            print(' ',end='')
+        for j in range(1,i+1):
+            print('* ',end='')
+        print('')
+    for i in range(x):
+        for k in range(x):
+            print('',end='')
+        for j in range(x-i,-1,-1):
+            print('* ',end='')
+        print('')
+
+pyramid(10)

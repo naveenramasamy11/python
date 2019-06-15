dcc67afb15c68821c68a0248a7fa80b84e216998
Naveen <naveenramasamy11@gmail.com>
2019-06-15 11:39:33 +0530

    python learning

--- classes/student.py
+++ classes/student.py
@@ -3,15 +3,15 @@ class Student:
     def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
-        print("Full name is", fname.capitalize() + ' ' + lname.capitalize())#, lname)
-    
+        print("Full name is", '{} {}'.format(fname.capitalize(),lname.capitalize()))
     def salary(self,rs):
         self.rs = rs
         rs = rs * 25 / 100
-        print('Salary increment for', self.fname.capitalize(),  'is:',rs)
+        print("Salary increment for", '{} {}'.format(self.fname.capitalize(),self.lname.capitalize()),  'is:',rs)
 
 std1 = Student('naveen', 'ramasamy')
 std2 = Student('roger', 'federer')
 std3 = Student('Peter', 'smith')
-std1.salary(1000)
+std1.salary(2000)
 std2.salary(3000)
+std3.salary(4000)

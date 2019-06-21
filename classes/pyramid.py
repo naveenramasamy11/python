f4b9051cb796f31d5d4a2e002a2892d3e47580bd
Naveen <naveenramasamy11@gmail.com>
2019-06-21 22:30:10 +0530

    python learning

---
+++ classes/pyramid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+class pyramid:
+    def __init__(self,Range,pattern,spaces):
+        self.Range = Range
+        self.pattern = pattern
+        self. spaces = spaces
+
+    def convert(self):
+        n = self.Range
+        for i in range(n):
+            print((n - i) * self.spaces, end='')
+            print(i * self.pattern)
+        for i in range(n,-1,-1):
+            print((n - i) * self.spaces, end = '')
+            print(i * self.pattern)
+abj1 = pyramid(10,' *',' ')
+abj1.convert()

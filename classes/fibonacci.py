4a5e9a3af230a522799f1cbd599c79901da979d1
Naveen <naveenramasamy11@gmail.com>
2019-06-28 00:04:36 +0530

    python learning

---
+++ classes/fibonacci.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+class Fibonacci:
+    def __init__(self,fn,sn):
+        self.fn = fn 
+        self.sn= sn 
+
+    def fib(self,n):
+        i = 0
+        while i < n:
+            thirdNumber = self.fn + self.sn
+            self.fn = self.sn
+            self.sn = thirdNumber
+            print(self.fn)
+            i += 1
+abj1 = Fibonacci(1,1)
+abj1.fib(10)
+
